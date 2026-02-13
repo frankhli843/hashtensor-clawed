fda9aec1a95bb3d53498d960a2794fe292c9da06
Frank Haolun Li <51806469+frankhli843@users.noreply.github.com>
2026-02-13 09:01:46 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,3 +5,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 while IFS= read -r -d '' file; do
   source "$file"
 done < <(find "$SCRIPT_DIR/scripts" -type f -name '*.sh' -print0)
+
+
+echo "HashTensor scripts sourced"
